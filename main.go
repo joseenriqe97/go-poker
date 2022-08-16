48346a44b200cea8b48e9df0600ea81419df382e
joseenriqe97 <joseenriqe92@gmail.com>
2022-08-16 01:47:01 -0400

    fixed

--- main.go
+++ main.go
@@ -7,80 +7,40 @@ import (
 )
 
 func main() {
-	fmt.Print(TestAlgorithm([]int{2, 3, 4, 5, 6}))
+	TestAlgorithm([]int{14, 5, 4, 2, 3})
 }
 
 func TestAlgorithm(cardsItem []int) (bool, string) {
 	var (
-		auxP     int
 		response string
+		count    int
 	)
-	//Para romper referencia
-	cards := make([]int, len(cardsItem))
-	copy(cards, cardsItem)
-
-	deckOfCards := make(map[string][]int)
-	brokeSequence := false
-
-	sort.Slice(cards, func(i, j int) bool {
-		return cards[i] < cards[j]
+	//TODO: It remains to validate the length of the array and that it is not less than 2
+	sort.Slice(cardsItem, func(i, j int) bool {
+		return cardsItem[i] < cardsItem[j]
 	})
-	for x := range cards {
 
-		if !brokeSequence && x < len(cards)-1 && cards[x]-cards[x+1] == -1 {
+	count = 1
 
-			if len(deckOfCards["firstHand"]) > 0 {
-				deckOfCards["firstHand"] = append(deckOfCards["firstHand"], cards[x+auxP])
-
-			} else {
-				deckOfCards["firstHand"] = append(deckOfCards["firstHand"], cards[x], cards[x+1])
-				auxP = x + 1
-			}
-
-		} else {
-			brokeSequence = true
-		}
-		if brokeSequence && x < len(cards)-1 && cards[x]-cards[x+1] == -1 {
-			auxP = x + 1
-			if len(deckOfCards["secondHand"]) > 0 && x < len(cards)-1 {
-				deckOfCards["secondHand"] = append(deckOfCards["secondHand"], cards[auxP+1])
-			} else {
-				deckOfCards["secondHand"] = append(deckOfCards["secondHand"], cards[x], cards[x+1])
-
-			}
-			if x == len(cards)-1 {
-				deckOfCards["secondHand"] = append(deckOfCards["secondHand"], cards[x])
-			}
+	for x := range cardsItem {
+		i := sort.Search(len(cardsItem), func(i int) bool { return cardsItem[i] >= cardsItem[x]+1 })
+		if i < len(cardsItem) && cardsItem[i] == cardsItem[x]+1 {
+			count = count + 1
+		} else if count < 4 {
+			count = 1
 		}
 	}
-	if len(deckOfCards["firstHand"]) == 5 {
-		response = arrayToString(cardsItem, ",", false)
+	if count == 4 && cardsItem[len(cardsItem)-1] == 14 && cardsItem[0] == 2 {
+		response = arrayToString(cardsItem, ",")
 		return true, response
 	}
-
-	if len(deckOfCards["secondHand"]) == 5 {
-		response = arrayToString(cardsItem, ",", false)
+	if count == 5 {
+		response = arrayToString(cardsItem, ",")
 		return true, response
 	}
-	if len(deckOfCards["firstHand"]) == 4 && cards[len(cards)-1] == 14 {
-
-		/* if deckOfCards["firstHand"][0] == 2 {
-			deckOfCards["firstHand"] = append(deckOfCards["firstHand"][:], cards[len(cards)-1])
-		} */
-
-		response = arrayToString(cardsItem, ",", false)
-		return true, response
-	}
-	response = arrayToString(cardsItem, ",", false)
-
-	return false, response
+	return false, arrayToString(cardsItem, ",")
 }
 
-func arrayToString(item []int, delim string, asc bool) string {
-	if asc {
-		sort.Slice(item, func(i, j int) bool {
-			return item[i] > item[j]
-		})
-	}
+func arrayToString(item []int, delim string) string {
 	return strings.Trim(strings.Replace(fmt.Sprint(item), " ", delim, -1), "[]")
 }
--- main_test.go
+++ main_test.go
@@ -9,20 +9,19 @@ import (
 func TestAlgorithmScenery(t *testing.T) {
 
 	res, cards := TestAlgorithm([]int{2, 3, 4, 5, 6})
-
 	assert.Equal(t, res, true)
 	assert.Equal(t, cards, "2,3,4,5,6")
 
 	res, cards = TestAlgorithm([]int{14, 5, 4, 2, 3})
 	assert.Equal(t, res, true)
-	assert.Equal(t, cards, "14,5,4,2,3")
+	assert.Equal(t, cards, "2,3,4,5,14")
 
 	res, cards = TestAlgorithm([]int{7, 7, 12, 11, 3, 4, 14})
 	assert.Equal(t, res, false)
-	assert.Equal(t, cards, "7,7,12,11,3,4,14")
+	assert.Equal(t, cards, "3,4,7,7,11,12,14")
 
 	res, cards = TestAlgorithm([]int{7, 3, 2})
 	assert.Equal(t, res, false)
-	assert.Equal(t, cards, "7,3,2")
+	assert.Equal(t, cards, "2,3,7")
 
 }
